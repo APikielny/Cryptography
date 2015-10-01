f3917bc0e9c3800002249eed985ef5ae72d5b212
APikielny <apikielny@gmail.com>
2015-10-01 17:18:56 -0400

    Updated from Brython Server: October 1, 2015 5:18:55 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -10,3 +10,12 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+inp=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+while inp!="e" and inp!="d" and inp!="q":
+    print("Did not understand command, try again. \n")
+    inp=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+message=input("Message: ")
+if inp=="e":
+    
+if inp=="q":
+    print("Goodbye!")
